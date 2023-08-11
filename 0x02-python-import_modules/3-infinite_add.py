dab5618505ad02321e091d982c9ab32cf802c303
gloriaadanna <gloriaagwam7@gmail.com>
2023-08-11 05:31:11 -0700

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """print the addition of all arguments."""
+    import sys
+    add_arg(sys.argv)
+    def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
