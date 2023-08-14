93de80779d9e960f623c7609b381aeb9915204cf
gloriaadanna <gloriaagwam7@gmail.com>
2023-08-14 03:37:00 -0700

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
+def print_list_integer(_list=[]):
+    for i in _list:
+        print('{:d}'.format(i))
